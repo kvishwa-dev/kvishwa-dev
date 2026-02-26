a7066ab1c79da4811bc19c77a8ed5360c804dee3
Vishwajeet Kumar <code.kvishwa@gmail.com>
2026-02-26 10:59:09 +0530

    casing issue

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from "react-router-dom"
-import ScrollToTop from "./components/ScrolltoTop";
+import ScrollToTop from "./components/ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
